8c45d09e05119e8e8d34b4043a423909fe4e4df2
Pablo <37676483+pablovihe@users.noreply.github.com>
2020-11-16 17:37:54 +0100

    Update and rename baliza_lcd_2.ino to code.ino

--- code.ino
+++ code.ino
@@ -13,17 +13,6 @@ LiquidCrystal lcd(2, 3, 4, 5, 6, 7);
 
 Adafruit_SGP30 sgp;
 
-/* return absolute humidity [mg/m^3] with approximation formula
-  @param temperature [°C]
-  @param humidity [%RH]
-*/
-uint32_t getAbsoluteHumidity(float temperature, float humidity) {
-  // approximation formula from Sensirion SGP30 Driver Integration chapter 3.15
-  const float absoluteHumidity = 216.7f * ((humidity / 100.0f) * 6.112f * exp((17.62f * temperature) / (243.12f + temperature)) / (273.15f + temperature)); // [g/m^3]
-  const uint32_t absoluteHumidityScaled = static_cast<uint32_t>(1000.0f * absoluteHumidity); // [mg/m^3]
-  return absoluteHumidityScaled;
-}
-
 void setup() {
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
@@ -49,8 +38,6 @@ void setup() {
     stemperatura();
     sgp30();
     lcd.clear();
-    // set the cursor to column 0, line 1
-    // (note: line 1 is the second row, since counting begins with 0):
     lcd.setCursor(0, 0);
     lcd.print("T:");
     lcd.setCursor(2, 0);
